641008bcad9811c5f7fa86319d8d1118b923ef0b
manhhv87 <67886698+manhhv87@users.noreply.github.com>
2024-08-20 15:53:15 +0700

    Update main_pretrain.py

--- main_pretrain.py
+++ main_pretrain.py
@@ -175,7 +175,7 @@ def main(args):
         model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu], find_unused_parameters=True)
         model_without_ddp = model.module
 
-    param_groups = optim_factory.add_weight_decay(model_without_ddp, args.weight_decay)
+    param_groups = optim_factory.param_groups_weight_decay(model_without_ddp, args.weight_decay)
     optimizer = torch.optim.AdamW(param_groups, lr=args.lr, betas=(0.9, 0.95))
     print(optimizer)
     loss_scaler = NativeScaler()
